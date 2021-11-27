7ef346b495cd8071133c2100b358650b634e6251
WeVouT <christoph.zeltner@softaware.at>
2021-11-27 23:09:22 +0000

    Deploying to gh-pages from @ WeVouT/IDesignYouBuildChallengeTwo@125b9d51c92e6d1caac07d529d5f6d0c8654ad9b 🚀

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ class HeaderComponent {
     }
 }
 HeaderComponent.ɵfac = function HeaderComponent_Factory(t) { return new (t || HeaderComponent)(); };
-HeaderComponent.ɵcmp = _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵdefineComponent"]({ type: HeaderComponent, selectors: [["app-header"]], decls: 17, vars: 0, consts: [[1, "flex", "flex-row", "justify-between", "w-full", "h-full", "gap-5", "p-6", "font-semibold", "text-white"], [1, "flex", "flex-col", "justify-center", "text-3xl"], [1, "flex-row", "hidden", "h-full", "md:flex", "gap-9"], [1, "flex", "flex-col", "justify-center"], [1, "flex", "flex-col", "justify-center", "h-full"], [1, "flex-col", "justify-center", "hidden", "h-full", "md:flex"], [1, "px-3", "py-2", "text-sm", "font-semibold", "rounded-lg", "bg-button-backdrop"], [1, "md:hidden"]], template: function HeaderComponent_Template(rf, ctx) { if (rf & 1) {
+HeaderComponent.ɵcmp = _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵdefineComponent"]({ type: HeaderComponent, selectors: [["app-header"]], decls: 21, vars: 0, consts: [[1, "flex", "flex-row", "justify-between", "w-full", "h-full", "gap-5", "p-6", "font-semibold", "text-white"], [1, "flex", "flex-col", "justify-center", "text-3xl"], [1, "flex-row", "hidden", "h-full", "md:flex", "gap-9"], [1, "flex", "flex-col", "justify-center"], [1, "flex", "flex-col", "justify-center", "h-full"], [1, "flex-col", "justify-center", "hidden", "h-full", "md:flex"], [1, "px-3", "py-2", "text-sm", "font-semibold", "rounded-lg", "bg-button-backdrop"], [1, "md:hidden"], ["width", "25", "height", "25", "viewBox", "0 0 19 16", "fill", "none", "xmlns", "http://www.w3.org/2000/svg"], ["d", "M0.665405 1.43602C0.665405 1.1406 0.782759 0.857281 0.991651 0.648389C1.20054 0.439498 1.48386 0.322144 1.77928 0.322144H17.3735C17.6689 0.322144 17.9522 0.439498 18.1611 0.648389C18.37 0.857281 18.4873 1.1406 18.4873 1.43602C18.4873 1.73143 18.37 2.01475 18.1611 2.22364C17.9522 2.43253 17.6689 2.54989 17.3735 2.54989H1.77928C1.48386 2.54989 1.20054 2.43253 0.991651 2.22364C0.782759 2.01475 0.665405 1.73143 0.665405 1.43602Z", "fill", "white"], ["d", "M0.665405 14.8025C0.665405 14.5071 0.782759 14.2237 0.991651 14.0148C1.20054 13.806 1.48386 13.6886 1.77928 13.6886H17.3735C17.6689 13.6886 17.9522 13.806 18.1611 14.0148C18.37 14.2237 18.4873 14.5071 18.4873 14.8025C18.4873 15.0979 18.37 15.3812 18.1611 15.5901C17.9522 15.799 17.6689 15.9163 17.3735 15.9163H1.77928C1.48386 15.9163 1.20054 15.799 0.991651 15.5901C0.782759 15.3812 0.665405 15.0979 0.665405 14.8025Z", "fill", "white"], ["d", "M8.4625 7.00537C8.16709 7.00537 7.88377 7.12273 7.67488 7.33162C7.46599 7.54051 7.34863 7.82383 7.34863 8.11924C7.34863 8.41466 7.46599 8.69798 7.67488 8.90687C7.88377 9.11576 8.16709 9.23311 8.4625 9.23311H17.3735C17.6689 9.23311 17.9522 9.11576 18.1611 8.90687C18.37 8.69798 18.4873 8.41466 18.4873 8.11924C18.4873 7.82383 18.37 7.54051 18.1611 7.33162C17.9522 7.12273 17.6689 7.00537 17.3735 7.00537H8.4625Z", "fill", "white"]], template: function HeaderComponent_Template(rf, ctx) { if (rf & 1) {
         _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementStart"](0, "div", 0);
         _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementStart"](1, "div", 1);
         _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵtext"](2, "UNX");
@@ -56,7 +56,14 @@ HeaderComponent.ɵcmp = _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵdefineCo
         _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementEnd"]();
         _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementEnd"]();
         _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementEnd"]();
-        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelement"](16, "div", 7);
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementStart"](16, "div", 7);
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵnamespaceSVG"]();
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementStart"](17, "svg", 8);
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelement"](18, "path", 9);
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelement"](19, "path", 10);
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelement"](20, "path", 11);
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementEnd"]();
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementEnd"]();
         _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementEnd"]();
     } }, styles: ["\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJoZWFkZXIuY29tcG9uZW50LnNjc3MifQ== */"] });
 
@@ -113,7 +120,7 @@ class MainComponent {
     }
 }
 MainComponent.ɵfac = function MainComponent_Factory(t) { return new (t || MainComponent)(); };
-MainComponent.ɵcmp = _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵdefineComponent"]({ type: MainComponent, selectors: [["app-main"]], decls: 7, vars: 0, consts: [[1, "w-full", "h-full", "bg-primary-backdrop", "font-inter"], [1, "absolute", "w-48", "h-48", "rounded-full", "md:h-bigBlur", "md:w-bigBlur", "left-1/6", "top-1/3", "bg-accent-backdrop", "filter", "blur-bigBlur"], [1, "flex", "flex-col", "w-full", "h-full"], [1, "h-20", "mb-8", "md:mb-28"], [1, "z-10", "h-1/3"], [1, "z-10", "flex", "flex-row", "justify-center", "w-full", "align-bottom"], ["src", "/assets/module.png", 1, "pt-15"]], template: function MainComponent_Template(rf, ctx) { if (rf & 1) {
+MainComponent.ɵcmp = _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵdefineComponent"]({ type: MainComponent, selectors: [["app-main"]], decls: 7, vars: 0, consts: [[1, "w-full", "h-full", "bg-primary-backdrop", "font-inter"], [1, "absolute", "w-48", "h-48", "rounded-full", "md:h-bigBlur", "md:w-bigBlur", "left-1/6", "top-1/3", "bg-accent-backdrop", "filter", "blur-bigBlur"], [1, "flex", "flex-col", "w-full", "h-full"], [1, "h-20", "mb-8", "md:mb-28"], [1, "z-10", "h-1/3"], [1, "z-10", "flex", "flex-row", "justify-center", "w-full", "px-5", "py-2", "mt-auto", "align-bottom"], ["src", "/assets/module.png", 1, "pt-15"]], template: function MainComponent_Template(rf, ctx) { if (rf & 1) {
         _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementStart"](0, "div", 0);
         _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelement"](1, "div", 1);
         _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementStart"](2, "div", 2);
@@ -217,7 +224,7 @@ class TitleComponent {
     }
 }
 TitleComponent.ɵfac = function TitleComponent_Factory(t) { return new (t || TitleComponent)(); };
-TitleComponent.ɵcmp = _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵdefineComponent"]({ type: TitleComponent, selectors: [["app-title"]], decls: 12, vars: 0, consts: [[1, "flex", "flex-row", "justify-center", "w-full", "h-full", "font-semibold", "text-center", "text-white"], [1, "flex", "flex-col", "w-full", "h-full", "gap-5"], [1, "text-5xl"], [1, "pb-2", "text-5xl", "text-transparent", "bg-gradient-to-r", "from-textStop", "to-textStart", "bg-clip-text"], [1, "flex", "flex-row", "justify-center"], [1, "w-5/6", "font-thin", "md:w-2/5"], [1, "px-8", "py-5", "font-bold", "bg-indigo-800", "rounded-lg"]], template: function TitleComponent_Template(rf, ctx) { if (rf & 1) {
+TitleComponent.ɵcmp = _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵdefineComponent"]({ type: TitleComponent, selectors: [["app-title"]], decls: 12, vars: 0, consts: [[1, "flex", "flex-row", "justify-center", "w-full", "h-full", "font-semibold", "text-center", "text-white"], [1, "flex", "flex-col", "w-full", "h-full", "gap-5", "px-5", "md:px-0"], [1, "text-5xl"], [1, "pb-2", "text-5xl", "text-transparent", "bg-gradient-to-r", "from-textStop", "to-textStart", "bg-clip-text"], [1, "flex", "flex-row", "justify-center"], [1, "w-5/6", "font-thin", "md:w-2/5"], [1, "px-8", "py-5", "font-bold", "bg-indigo-800", "rounded-lg"]], template: function TitleComponent_Template(rf, ctx) { if (rf & 1) {
         _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementStart"](0, "div", 0);
         _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementStart"](1, "div", 1);
         _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementStart"](2, "span", 2);
